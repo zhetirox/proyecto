2e652077011b1c14654d95fbfd1900500fa5ef33
Daniel Galindo <gith36877@gmail.com>
2025-06-19 20:56:10 -0500

    Se agrego un menu de Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Mapa from './pages/Mapa/Mapa';
+import DropdownMenu from './components/Menu/DropdownMenu';
 
 function App() {
   return (
     <>
       <Mapa/>
+      <DropdownMenu/>
     </>
   );
 }

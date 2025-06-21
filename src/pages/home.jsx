1620e76c429a71bbf7faf32c2c1d0a719a513538
valentina <valentina07colorado@gmail.com>
2025-06-20 20:48:51 -0500

    sibuu

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,8 @@ import { Button } from 'primereact/button';
 import { Checkbox } from 'primereact/checkbox';
 import { Password } from 'primereact/password';
 import { Message } from 'primereact/message';
-import { Logo } from './img/logo.png';
+import Sibu from "./Sibu.png"
+
 
 function Home() {
 
@@ -15,8 +16,7 @@ function Home() {
 
     return (
 
-        <div className="card flex justify-content-center">
-            <Image src="https://primefaces.org/cdn/primereact/images/galleria/galleria7.jpg" alt="Image" width="250" />
+       
 
             <div className='home-container'>
                 <div className='home-box'>
@@ -36,17 +36,10 @@ function Home() {
                             </div>
 
                             <div>
-                                <FloatLabel>
-                                    <InputText id="username" value={value} onChange={(e) => setValue(e.target.value)} />
-                                    <label htmlFor="username">Contraseña</label>
-                                </FloatLabel>
                                 <Password />
                             </div>
-                            <Button label="Sumbit" />
+                            <Button label="Ingresar" />
 
-                            <div className="checkbox">
-                                <Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox>
-                            </div>
 
                             <p className="registro">
                                 Olvido contraseña <a href="#">Olvido contraseña</a>
@@ -56,15 +49,13 @@ function Home() {
 
                 </div>
                 <div className="logo">
-
-                    <img src={logo} alt='Logo de mi página' />
-
-
+                    <img src={Sibu}  />
+                   
                 </div>
             </div>
-        </div>    
+           
             )
 }
 
 
-            export default Home;
+export default Home;

ba40478a61575be0b35cf77a4a842b45f3903847
keiner <morakeiner2830@gmail.com>
2025-06-24 20:47:48 -0500

    registro e iniciosesion

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import { Button } from "primereact/button";
 import Home from "./pages/home";
+import Registro from "./page/registro/registro";
 
 function App() {
     return (
         <PrimeReactProvider>
-            <Home />
+           
+            <Registro/>
         </PrimeReactProvider>
 
     );
--- src/page/registro/registro.jsx
+++ src/page/registro/registro.jsx
@@ -1,14 +1,26 @@
-import React from "react";
-import './registro.css';
+import React from 'react';
+import { Button } from 'primereact/button';
+import './registro.css'
+import sibu_redondo from './logo.png'
 
-
- function Registro() {
+export default function Registro() {
     return (
-      <>
-      </>
-    )
+        <>
+            <div className='logo'>
+                <img src={sibu_redondo} alt='' />
+            </div>
+            <div className='contenedor'>
+
+                <div className="registro">
+                    <Button label="REGISTRARSE" />
+                </div>
+                <p>ó</p>
+                <div className="inicio">
+                    <Button label="INICIAR SESIÓN" />
+                </div>
+            </div>
+        </>
+
+    );
 }
-        
-
-
 

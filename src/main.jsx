9955a5fc6832c0b25c732a86536eec836019dd5a
Daniel Galindo <gith36877@gmail.com>
2025-07-03 19:03:19 -0500

    correccion conflictos main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);

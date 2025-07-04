f0691ab3b3037ad8bacb765ee738fac687db8d45
Daniel Galindo <gith36877@gmail.com>
2025-07-03 19:16:26 -0500

    conflicto libreria icons

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 

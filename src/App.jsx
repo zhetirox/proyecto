75f31cff376f96c5ec9947e6ab8ee6a9c8c9accd
Daniel Galindo <gith36877@gmail.com>
2025-07-10 19:46:04 -0500

    ajuste de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import Login from "./pages/Login/Login";
+import Mapa from "./pages/Mapa/Mapa";
+import SideMenu from './components/Menu/SideMenu';
 
 function App() {
     return (
         <PrimeReactProvider>
+          <Mapa/>
+          <SideMenu/>
             <Login />
         </PrimeReactProvider>
 
